477722583b09adb11cc3f0f03b7f147bf0f4efe7
Sandeep Dewangan <sandeepinwork@gmail.com>
2026-04-10 16:48:48 +0530

    Users Table Creation

--- TodoApp/models.py
+++ TodoApp/models.py
@@ -1,5 +1,19 @@
 from database import Base
-from sqlalchemy import Column, Integer, String, Boolean
+from sqlalchemy import Column, ForeignKey, Integer, String, Boolean
+
+
+class Users(Base):
+    __tablename__ = 'users'
+
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String, unique=True)
+    username = Column(String, unique=True)
+    first_name = Column(String)
+    last_name = Column(String)
+    hashed_password = Column(String)
+    is_active = Column(Boolean, default=True)
+    role = Column(String)
+
 
 class Todo(Base):
     __tablename__ = 'todos'
@@ -9,5 +23,6 @@ class Todo(Base):
     description = Column(String)
     priority = Column(Integer)
     complete = Column(Boolean, default=False)
+    owner_id = Column(Integer, ForeignKey("users.id"))
     
 
