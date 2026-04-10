f8237111ef50d51fea6a65cf2e9cb3e0456e7b38
Sandeep Dewangan <sandeepinwork@gmail.com>
2026-04-10 17:43:24 +0530

    Create and save new user

--- TodoApp/routers/auth.py
+++ TodoApp/routers/auth.py
@@ -1,8 +1,45 @@
-from fastapi import APIRouter
+from typing import Annotated
+
+from fastapi import APIRouter, Depends
+from pydantic import BaseModel
+from sqlalchemy.orm import Session
+from starlette import status
+
+from database import SessionLocal
+from models import Users
 
 router =  APIRouter()
 
 
-@router.get("/auth")
-def get_users():
-    return {'user': 'Sandeep'}
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+db_dependency = Annotated[Session, Depends(get_db)]
+
+class CreateUserRequest(BaseModel):
+    email:str
+    username:str
+    first_name:str
+    last_name:str
+    password:str
+    role:str
+
+@router.post("/auth", status_code=status.HTTP_201_CREATED)
+def create_user(db: db_dependency, user_req: CreateUserRequest):
+    user = Users(
+        email = user_req.email,
+        username = user_req.username,
+        first_name = user_req.first_name,
+        last_name = user_req.last_name,
+        hashed_password = user_req.password,
+        role = user_req.role,
+        is_active=True
+    )
+    db.add(user)
+    db.commit()
+
