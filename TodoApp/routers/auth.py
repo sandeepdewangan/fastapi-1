db00717026e10a5c4f75cc5c36c7a21a01c92f15
Sandeep Dewangan <sandeepinwork@gmail.com>
2026-04-22 13:48:01 +0530

    Authentication: Encode JWT and Send to Client

--- TodoApp/routers/auth.py
+++ TodoApp/routers/auth.py
@@ -1,3 +1,4 @@
+from datetime import datetime, timedelta, timezone
 from typing import Annotated
 
 from fastapi import APIRouter, Depends
@@ -9,8 +10,14 @@ from database import SessionLocal
 from models import Users
 
 from fastapi.security import OAuth2PasswordRequestForm
+from jose import jwt
 
 router =  APIRouter()
+
+# Generate random secret key
+# openssl rand -hex 32
+SECRET_KEY = '51a53e78df31dec029ca6d3659103b26fbf504555ff2520f6cda90faf89ef9f6'
+ALGORITHM = 'HS256'
 
 
 def get_db():
@@ -31,11 +38,21 @@ class CreateUserRequest(BaseModel):
     password:str
     role:str
 
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
 def authenticate_user(username:str, password:str, db):
     user = db.query(Users).filter(Users.username == username).first()
     if not user:
         return False
-    return True
+    return user
+
+def create_access_token(username:str, user_id:int, expires_delta:timedelta):
+    encode = {'sub':username, 'id':user_id}
+    expires = datetime.now(timezone.utc) + expires_delta
+    encode.update({'exp': expires})
+    return jwt.encode(encode, SECRET_KEY, algorithm=ALGORITHM)
 
 @router.post("/auth", status_code=status.HTTP_201_CREATED)
 def create_user(db: db_dependency, user_req: CreateUserRequest):
@@ -51,10 +68,11 @@ def create_user(db: db_dependency, user_req: CreateUserRequest):
     db.add(user)
     db.commit()
 
-@router.post("/token")
+@router.post("/token", response_model=Token)
 def login_for_acces_token(form_data: Annotated[OAuth2PasswordRequestForm, Depends()], db:db_dependency):
     user = authenticate_user(form_data.username, form_data.password, db)
     if not user:
         return 'Failed Authentication'
-    return'Successfull Authentication'
+    token = create_access_token(user.username, user.id, timedelta(minutes=20))
+    return {'access_token': token, 'token_type':'bearer'}
 
